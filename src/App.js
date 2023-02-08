e02b3b7b8160a6ff6a7b21f3ed7b63ae45c9bde2
SheihMaks <92758256+SheihMaks@users.noreply.github.com>
2023-02-09 01:23:20 +0300

    +

--- src/App.js
+++ src/App.js
@@ -6,7 +6,6 @@ function App() {
       <div className="wrapper__overlay">
         <div className="overlay__drawer">
           <h2>Корзина</h2>
-          <div className="drawer__content d-flex flex-column justify-between">
           <ul className="drawer__list">
             <li className="drawer__item">
               <img className="item__img" src="/img/Sneakersimg.jpg" alt="sneakers" width={70} height={70} />
@@ -15,19 +14,29 @@ function App() {
                 <b>8499 грн.</b>
               </div>
               <img  className="item__removeBtn" src="/img/deleteButton.svg" alt="delete" width={32} height={32}/>
+              </li>
+              <li className="drawer__item">
+              <img className="item__img" src="/img/Sneakersimg.jpg" alt="sneakers" width={70} height={70} />
+              <div className="sneakers__info">
+                <p className="mb-10">Мужские Кроссовки Nike Air Max 270</p>
+                <b>8499 грн.</b>
+              </div>
+              <img  className="item__removeBtn" src="/img/deleteButton.svg" alt="delete" width={32} height={32}/>
             </li>
           </ul>
-          <div>
-      <div className="d-flex align-center justify-between">
+          <ul className="drawer__totalBlock">
+      <li className="totalBlock__item">
                 <p>Итого:</p>
-                <b>15789 грн..</b>
-              </div>
-              <div className="d-flex align-center justify-between">
-              <p>Налог %:</p>
+                <div></div>
+                <b>15789 грн.</b>
+              </li>
+              <li className="totalBlock__item">
+                <p>Налог 5%:</p>
+                <div></div>
                 <b>2789 грн.</b>
-                </div>
-            </div>
-        </div>
+                </li>
+          </ul>
+          <button className="drawer__orderBtn" type="button">Оформить заказ</button>
       </div>
     </div>
           <header className="d-flex justify-between align-center p-40" >
