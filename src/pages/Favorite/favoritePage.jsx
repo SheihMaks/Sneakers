48bbde917f2ec647774ea617f71f9cd975650c29
SheihMaks <92758256+SheihMaks@users.noreply.github.com>
2023-03-08 04:13:13 +0300

    +

--- src/pages/Favorite/favoritePage.jsx
+++ src/pages/Favorite/favoritePage.jsx
@@ -5,5 +5,16 @@ const FavoritePage=()=>{
     const [favoritesList, setFavorites]=useState(()=>{
         return JSON.parse(localStorage.getItem('favoritesList')) ?? []
     })
-    return(<div className="d-flex justify-between align-center mb-40">><h1>{Мои закладки}</h1><SearchBox/></div>)
+
+    const [searchFavorites,setSearchFavorites]=useState('')
+
+    const onSearchFavorites=(event)=>{
+    setSearchFavorites(event.target.value)
+    }
+
+    return(<div className="d-flex justify-between align-center mb-40">
+        <h1>{searchFavorites ? `Идет поиск по запросу: "${searchValue}"` : Мои закладки}</h1>
+        <SearchBox
+        searchValue={searchFavorites}
+        onSearchValue={onSearchFavorites}/></div>)
 }
